c2a4db3436bdf77d935d3ba6eb59c520498a96ae
Naohiro KORIYAMA <nkoriyama@gmail.com>
2026-04-26 14:09:41 +0900

    chore: リポジトリからビルドファイルを削除し、追跡対象外にする

--- build_wolfssl/unit_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/var/home/bazzite/ドキュメント/src/nkCryptoTool/build_wolfssl/unit_tests[1]_tests.cmake")
-  include("/var/home/bazzite/ドキュメント/src/nkCryptoTool/build_wolfssl/unit_tests[1]_tests.cmake")
-else()
-  add_test(unit_tests_NOT_BUILT unit_tests_NOT_BUILT)
-endif()
